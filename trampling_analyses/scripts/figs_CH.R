467ea9702e950b7b71af95e50dc2f1ac09d1a4f1
Carly Hilbert <chilbe02@student.ubc.ca>
2022-12-05 11:20:57 -0800

    Updated color on height/dist and width/dist graph, started working on phylogeny timeline.

--- trampling_analyses/scripts/figs_CH.R
+++ trampling_analyses/scripts/figs_CH.R
@@ -9,13 +9,14 @@
 library(ggplot2)
 library(ggsignif)
 
-
 rm(list=ls()) 
 
 
 # # WORKING DIRECTORIES # #
-comp_dat <- '~/Desktop/Code/Garibaldi/trampling_analyses/compiled_data/' #WD for NC
-figs <- '~/Desktop/Code/Garibaldi/trampling_analyses/figures/BC_PARF/' #WD for NC
+# comp_dat <- '~/Desktop/Code/Garibaldi/trampling_analyses/compiled_data/' #WD for NC
+# figs <- '~/Desktop/Code/Garibaldi/trampling_analyses/figures/BC_PARF/' #WD for NC
+comp_dat <- "C:/Users/Carly/Documents/Code/Garibaldi/trampling_analyses/compiled_data" # WD Carly
+figs <- "C:/Users/Carly/Documents/Code/Garibaldi/trampling_analyses/figures/BC_PARF" #WD Carly
 
 
 # # INPUT FILES # #
@@ -50,13 +51,14 @@ dat[ff] <- lapply(dat[ff], as.factor)
 setwd(figs) #location to save figures
 
 mytheme <-   theme_classic() +
-  theme(axis.text.x = element_text(colour = 'black', size = 20), #x axis text size
-        axis.text.y = element_text(colour = 'black', size = 25), #y axis text size
-        axis.title.x = element_text(size = 28), #x axis label size
-        axis.title.y = element_text(size = 28), #y axis label size
+  theme(axis.text.x = element_text(colour = 'black', size = 10), #x axis text size
+        axis.text.y = element_text(colour = 'black', size = 10), #y axis text size
+        axis.title.x = element_text(size = 18), #x axis label size
+        axis.title.y = element_text(size = 18), #y axis label size
         plot.title = element_text(size = 30, #title size
                                   hjust = 0.5), #align title to center
-        legend.title = element_text(size = 24), legend.text = element_text(size = 22)) 
+        legend.title = element_text(size = 24), #Legend title text size
+        legend.text = element_text(size = 22)) #Actual legend text
 
 
 # # PLOTS
@@ -82,14 +84,15 @@ ggplot(dat, aes(x, y, color = dist)) +
 species_names <- c('carspp'= "Carex spp.", 'casmer' = "Cassiope mertensiana", 'phyemp' = "Phyllodoce empetriformis", 'phygla' = "Phyllodoce glanduliflora", 'vacova' = "Vaccinium ovalifolium")
 
 gplot <- ggplot(dat, aes(x=dist, y=height_mm, fill=dist)) +
-  geom_boxplot() +
+  geom_boxplot(fill=c('darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2')) +
   facet_grid(.~ species, labeller = as_labeller(species_names)) + #separate by species and rename with full species names
   # geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars --> cannot run Wilcox test with nested sampling design
   scale_x_discrete(labels=c("0" = "Off Trail", "1" = "On Trail")) + #rename dist variable from 0 and 1
   theme(axis.text.x = element_text(angle = 60, hjust=1)) + #angle text
   ylab("Height (mm)") +
   theme(legend.position = "none") + #no legend 
-  mytheme
+  mytheme +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"))
 gplot
 
 ggsave(gplot, filename = 'height_dist.pdf')
@@ -97,14 +100,17 @@ ggsave(gplot, filename = 'height_dist.pdf')
 
 # max diameter boxplot of disturbance vs no distubance by species
 
+species_names <- c('carspp'= "Carex spp.", 'casmer' = "Cassiope mertensiana", 'phyemp' = "Phyllodoce empetriformis", 'phygla' = "Phyllodoce glanduliflora", 'vacova' = "Vaccinium ovalifolium")
+
 widthplot <- ggplot(dat, aes(x=dist, y=mxdiam_mm, fill=dist)) +
-  geom_boxplot() +
+  geom_boxplot(fill=c('darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2', 'darkolivegreen', 'steelblue2')) + # colour palette
   facet_grid(.~ species, labeller = as_labeller(species_names)) + #separate by species and rename with full species names
-  geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
+  # geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
   scale_x_discrete(labels=c("0" = "Off Trail", "1" = "On Trail")) + #rename dist variable from 0 and 1
   theme(axis.text.x = element_text(angle = 60, hjust=1)) + #angle text
   ylab("Maximum diameter (mm)") +
   theme(legend.position = "none") #no legend
+  mytheme
 widthplot
 
 ggsave(widthplot, filename= 'width_dist.pdf')
@@ -115,7 +121,7 @@ ggsave(widthplot, filename= 'width_dist.pdf')
 budplot <- ggplot(dat, aes(x=dist, y=buds, fill=dist)) +
   geom_boxplot() +
   facet_grid(.~ species, labeller = as_labeller(species_names)) + #separate by species and rename with full species names
-  geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
+  # geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
   scale_x_discrete(labels=c("0" = "Off Trail", "1" = "On Trail")) + #rename dist variable from 0 and 1
   theme(axis.text.x = element_text(angle = 60, hjust=1)) + #angle text
   ylab("Buds") +
@@ -129,7 +135,7 @@ ggsave(budplot, filename = 'bud_dist.pdf')
 flowersplot <- ggplot(dat, aes(x=dist, y=flws, fill=dist)) +
   geom_boxplot() +
   facet_grid(.~ species, labeller = as_labeller(species_names)) + #separate by species and rename with full species names
-  geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
+  # geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
   scale_x_discrete(labels=c("0" = "Off Trail", "1" = "On Trail")) + #rename dist variable from 0 and 1
   theme(axis.text.x = element_text(angle = 60, hjust=1)) + #angle text
   ylab("Flowers") +
@@ -143,7 +149,7 @@ ggsave(flowersplot, filename = 'flowers_dist.pdf')
 ggplot(dat, aes(x=dist, y=frts, fill=dist)) +
   geom_boxplot() +
   facet_grid(.~ species, labeller = as_labeller(species_names)) + #separate by species and rename with full species names
-  geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
+  # geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
   scale_x_discrete(labels=c("0" = "Off Trail", "1" = "On Trail")) + #rename dist variable from 0 and 1
   theme(axis.text.x = element_text(angle = 60, hjust=1)) + #angle text
   ylab("Fruits") +
@@ -157,8 +163,19 @@ dat$BudsFlwsFrts <- rowSums(dat[ , c(5,6,7)])
 ggplot(dat, aes(x=dist, y=BudsFlwsFrts, fill=dist)) +
   geom_boxplot() +
   facet_grid(.~ species, labeller = as_labeller(species_names)) + #separate by species and rename with full species names
-  geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
+  # geom_signif(comparisons = list(c("0", "1")), map_signif_level=TRUE) + #significance stars
   scale_x_discrete(labels=c("0" = "Off Trail", "1" = "On Trail")) + #rename dist variable from 0 and 1
   theme(axis.text.x = element_text(angle = 60, hjust=1)) + #angle text
   ylab("Reproductive structures") +
   theme(legend.position = "none") #no legend
+
+# # Below is my best attempt for my weird graph # #
+
+dat$Buds
+ggplot(dat, aes(BudsPerSpecies)) +
+ggplot(dat, aes(FlwsPerSpecies)) +
+ggplot(dat, aes (FrtsPerSpecies)) +
+  geom_bar() +
+  facet_grid(.~ species, labeller = as_labeller(species_names)) + 
+  theme_minimal() +
+  labs(y = 'test label', title = 'test title')
